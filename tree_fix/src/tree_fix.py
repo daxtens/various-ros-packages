ae9d97d87769dbc80024c7864b9bffc40b9a7711
Daniel Axtens <daniel@axtens.net>
2014-09-28 14:08:47 +1000

    save

--- tree_fix/src/tree_fix.py
+++ tree_fix/src/tree_fix.py
@@ -19,12 +19,15 @@ SAME_TREE_THRESH_MATCH = 3
 
 # how far can a tree be from existing trees after stabilisation
 # before we conclude it's a new tree
-# should be lower than the other threshold
 SAME_TREE_THRESH_DETECT = 3
 
 # If tree trunks move by up to this much we update our static points
 # so that the next scan has a lower error.
 SAME_TREE_THRESH_UPDATE=0.3
+
+#
+SCAN_MATCH_WINDOW = 120 * 3.1415/180
+SCAN_MATCH_PTS = 360
 
 def ptdistance(a, b):
     return distance((a.x,a.y),(b.x,b.y))
@@ -58,54 +61,108 @@ def rigid_transform_3D(A, B):
 
     # special reflection case
     if numpy.linalg.det(R) < 0:
-       #print "Reflection detected"
-       raise Exception
+        #print("Reflection detected")
+        Rx = (numpy.matrix([[1,0],[0,-1]]))
+        Ry = (numpy.matrix([[-1,0],[0,1]]))
+        # Future self: this is where you're up to.
+        
+        #  you're getting an offset triangle
+        # you vaguely suspect you're seeing a double reflection, because you have close to an equilateral triangle. A double reflect would be indistinguisable - det() wise, from a non-reflected one.
+
+        # re detecting and correctin reflections:
+        # Both reflect x and reflect y are possible mathematically.
+        # You are probably going to be able to tell on the basis of
+        # R[0,0] - on either side of theta = 0, R[0,0]>0
+        # but - if new pts = oldpts * (reflection * rotation) + translation
+        # it's not the case that (true rotation) = (reflection * rotation) * (inverse reflection)
+        #if numpy.linalg.det(Rx) > 0:
+        #    R=Rx
+        #elif numpy.linalg.det(Ry) > 0:
+        #    R = Ry
+        #else:
+        #    print "unfixable reflection"
+        #    raise Exception
+        raise Exception
     #   Vt[2,:] *= -1
     #   R = Vt.T * U.T
 
     t = -R*centroid_A.T + centroid_B.T
 
     #print t
-
     return R, t
 
+def error(A, B):
+    err = A - B
+    err = numpy.multiply(err, err)
+    err = numpy.sum(err)/len(A)
+    return math.sqrt(err)
+
+
+def rototransform2D(A, B):
+    # try a range of angles around 0 to find the best fit
+    assert len(A) == len(B)
+
+    N = A.shape[0]; # total points
+
+    centroid_A = numpy.mean(A, axis=0)
+    centroid_B = numpy.mean(B, axis=0)
+    
+    # centre the points
+    AA = A - numpy.tile(centroid_A, (N, 1))
+    BB = B - numpy.tile(centroid_B, (N, 1))
+
+    # try a set of rotations
+    minerr = 999999999
+    best_yaw = 0
+    for yaw in numpy.linspace(-SCAN_MATCH_WINDOW, SCAN_MATCH_WINDOW,
+                              SCAN_MATCH_PTS):
+        R = numpy.matrix([[math.cos(yaw), -math.sin(yaw)],
+                          [math.sin(yaw), math.cos(yaw)]])
+        B2 = BB * R
+        #t = -R*centroid_A.T + centroid_B.T
+        #BC = ((R*B.T)+numpy.tile(t,(1, len(A)))).T
+        err = error(B2, AA)
+        #normt = numpy.linalg.norm(t)
+        #print(err, normt)
+        if err < minerr:
+            minerr = err
+            best_yaw = yaw
+
+    yaw = best_yaw
+    R = numpy.matrix([[math.cos(yaw), -math.sin(yaw)],
+                      [math.sin(yaw), math.cos(yaw)]])
+    t = -R*centroid_A.T + centroid_B.T
+
+    #print(R,t)
+    return R, t
+    
 
 tf_listener = None
+tf_publisher = None
 pc_publisher = None
 pc2_publisher = None
 odom_publisher = None
 static_pts = []
-# http://docs.ros.org/api/sensor_msgs/html/msg/LaserScan.html
+last_pos = None
+last_yaw = None
+
+world_frame = '/world'
 
 def callback(trees_scan):
-    global static_pts, position, yaw
-    tf_broadcaster = tf.TransformBroadcaster()
-
+    global static_pts, last_pos, last_yaw, tf_publisher
+    start = rospy.Time.now()
     # get the scan
     points_xyz = pc2.read_points(trees_scan)
     scan_points = [(x, y) for (x, y, z) in points_xyz]
 
-
     if len(scan_points) == 0:
+        print('0', rospy.Time.now()-start)
         return
 
-
-    #print points
-
-    ## get the scan->world transform
-    #try:
-    #    (trans,rot) = tf_listener.lookupTransform('/world','/base_link', rospy.Time(0))
-    #except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException) as e:
-    #    print(e)
-    #    return
-    
-    #print('t: ', trans)
-    #print('r: ', rot)
-
-    # convert points to world FoR
+    # convert points to uncorrected world (odom) FoR
     # this is probably a terrible way to do this. Oh well.
     scan_pts_in_world = [tf_listener.transformPoint(
-                             '/world',
+                             '/odom',
                              PointStamped(trees_scan.header,
                                           Point(p[0],p[1],0)))
                          for p in scan_points]
@@ -119,17 +176,21 @@ def callback(trees_scan):
     # again, horrible way to do this
     common_sp = []
     common_np = []
+    used = [False] * len(static_pts)
     for p in scan_pts_in_world:
         found = False
         mindist = SAME_TREE_THRESH_MATCH
-        for sp in static_pts:
+        for (i, sp) in enumerate(static_pts):
             #print(p,sp)
             dist = distance((p.point.x, p.point.y),
-                            sp) 
-            if dist < mindist:  
+                            sp)
+            if dist < mindist and used[i]:
+                print('already used sp', i)
+            if dist < mindist and not used[i]:  
                 found = True
                 bestpoint = sp
                 mindist = dist
+                used[i] = True
         
         if found:
             common_sp += [bestpoint]
@@ -140,7 +201,6 @@ def callback(trees_scan):
     ## Match our position and orientation based on common pts
     ## the idea is New = Static*R + t + N
     publish_pose = True
-    theta_cov = 1e-6
     if len(common_sp) == 0:
         # we have nothing. Don't publish a pose.
         R = numpy.identity(2)
@@ -160,11 +220,11 @@ def callback(trees_scan):
         # it claims to be 3d but works for 2d too
         staticp = numpy.matrix(common_sp)
         newp = numpy.matrix(common_np)
-        try:
-            R, t = rigid_transform_3D((newp),
-                                      (staticp))
-        except:
-            return
+        #try:
+        R, t = rototransform2D((newp),
+                                   (staticp))
+        #except:
+        #    return
         
                     
     # now we transform all the world points with R and t
@@ -175,67 +235,102 @@ def callback(trees_scan):
     rtd = rd + numpy.tile(t, (1, len(scan_pts_in_world)))
     corrected_pts_xy = rtd.T
 
-    # also error
-    err = corrected_pts_xy - scan_pts_in_world_xy
-    err = numpy.multiply(err, err)
-    err = numpy.sum(err)/len(corrected_pts_xy)
-    rmse = math.sqrt(err)
-
-    # we're occasionally experiencing big jumps
-    # if we get one, drop this entire match - don't look
-    # for new trees
-    normt = numpy.linalg.norm(t)
-    yaw = math.asin(R[1,0])
-    #print(normt, rmse)
-    if normt > 4 or rmse > 4 or yaw > 3.14/3:
-        print('dropping', normt, rmse, yaw, t)
-
-    # detect new/moved trees
-    for p in numpy.array(corrected_pts_xy):
-        for (i,sp) in enumerate(static_pts):
-            found = False
-            dist = distance(p, sp)
-            #if dist < SAME_TREE_THRESH_UPDATE:
-            #    static_pts[i] = p
-            #    found = True
-            #    break
-            if dist < SAME_TREE_THRESH_DETECT:
-                found = True
-                break
-        
-        if not found:
-            print('new tree at: ', p)
-            static_pts += [p]
-
     # publish detected and matched trees
     points_xyz = [(x,y,0) for (x, y) in static_pts]
     cloud = PointCloud2()
-    cloud.header.frame_id = "world"
+    cloud.header.frame_id = world_frame
     cloud = pc2.create_cloud_xyz32(cloud.header, points_xyz)
     pc_publisher.publish(cloud)
 
     points_xyz = [(x,y,0) for (x, y) in 
                   numpy.array(corrected_pts_xy)]
     cloud = PointCloud2()
-    cloud.header.frame_id = "world"
+    cloud.header.frame_id = world_frame
     cloud = pc2.create_cloud_xyz32(cloud.header, points_xyz)
     pc2_publisher.publish(cloud)
 
-    # publish the result as odometry (? todo maybe a Pose?)
-    if not publish_pose:
+
+    # also error
+    if len(common_np) >= 2:
+        rd = R*numpy.matrix(common_np).T
+        rtd = rd + numpy.tile(t, (1, len(common_np)))
+        corrected_common_new_pts = rtd.T
+        rmse = error(corrected_common_new_pts,
+                     numpy.matrix(common_sp))
+
+        rmse_uncorr = error(numpy.matrix(common_np),
+                            numpy.matrix(common_sp))
+        #print(rmse, rmse_uncorr)
+        if rmse > rmse_uncorr:
+            print(rmse,rmse_uncorr)
+
+    ## we're occasionally experiencing big jumps
+    ## if we get one, drop this entire match
+    normt = numpy.linalg.norm(t)
+    yaw = math.atan2(R[1,0],R[0,0])
+    #print(normt, rmse)
+    #if normt > 10 or rmse > 4 or yaw > 3.4/3:
+    drop=False
+    if normt > 10:
+        print('n')
+        drop = True
+    if rmse > 10:
+        print('e')
+        drop = True
+    if abs(yaw) > 3.14/2:
+        print('y')
+        drop = True
+    if drop:
+        tf_publisher.sendTransform(last_pos,
+                                   quaternion_from_euler(0,0,last_yaw),
+                                   rospy.Time.now(),
+                                   '/odom',
+                                   world_frame)
+        print('d', rospy.Time.now()-start)
         return
+
+    # detect new/moved trees
+    if rmse < 1:
+        for p in numpy.array(corrected_pts_xy):
+            for (i,sp) in enumerate(static_pts):
+                found = False
+                dist = distance(p, sp)
+                if dist < SAME_TREE_THRESH_UPDATE:
+                    static_pts[i] = p
+                    found = True
+                    break
+                if dist < SAME_TREE_THRESH_DETECT:
+                    found = True
+                    break
+        
+            if not found:
+                print('new tree at: ', p)
+                static_pts += [p]
+    else:
+        print('td skipped', rmse)
+
+    # publish the result as odometry & transform
+    if not publish_pose:
+        tf_publisher.sendTransform(last_pos,
+                                   quaternion_from_euler(0,0,last_yaw),
+                                   rospy.Time.now(),
+                                   "/odom",
+                                   world_frame)
+        print('p', rospy.Time.now()-start)
+        return
+
     msg = Odometry()
-    msg.header.frame_id = '/world'
+    msg.header.frame_id = 'odom'
     #msg.child_frame_id='/base_link'
     msg.header.stamp = rospy.Time.now()
-    # where are we in the map frame? transform 0,0 to find out
+    # where are we in the map frame? transform (0,0)+t to find out
     abs_pos = tf_listener.transformPoint(
-        '/world',
+        world_frame,
         PointStamped(trees_scan.header,
-                     Point(0,0,0)))
+                     Point(t[0],t[1],0)))
 
     orientation = tf_listener.transformQuaternion(
-        '/world',
+        'odom',
         QuaternionStamped(trees_scan.header,
                           Quaternion(*quaternion_from_euler(0,0,yaw))))
 
@@ -249,16 +344,45 @@ def callback(trees_scan):
                            0,    0,    0,     0,     0,     1e-2]
 
     odom_publisher.publish(msg)
+    
+    # TF hierarchy (per REP 105, iirc) is:
+    #     map -> odom -> base_link
+    # we want to publish a transform that is map -> odom
+    # such that (map -> odom) + (odom -> baselink) is correct
+    # we know that the offset that baselink requires is t, R
+    # so we publish odom to be at (t.x, t.y) and (0,0,yaw)
+    # which will give (map -> odom) + (odom->baselink)
+    # equal to (map->uncorrected baselink+(t,R))
+
+    # average the current and the last
+    # reduce the effect of large slightly wrong jumps
+    last_pos = ((last_pos[0]*0.5 + t[0]*0.5), (last_pos[1]*0.5 + t[1]*0.5), 0)
+    last_yaw = (last_yaw+yaw)/2
+    tf_publisher.sendTransform(last_pos,
+                               quaternion_from_euler(0,0,last_yaw),
+                               rospy.Time.now(),
+                               "/odom",
+                               world_frame)
+    print('e', rospy.Time.now()-start)
 
 
 def listener():
-    global pc_publisher,pc2_publisher, tf_listener, odom_publisher
+    global pc_publisher,pc2_publisher, tf_listener, odom_publisher, \
+        tf_publisher, last_pos, last_yaw
     rospy.init_node('tree_fix', anonymous=False)
     rospy.Subscriber("trees", PointCloud2, callback)
     pc_publisher = rospy.Publisher("static_trees", PointCloud2)
     pc2_publisher = rospy.Publisher("corrected_trees", PointCloud2)
     tf_listener = tf.TransformListener()
     odom_publisher = rospy.Publisher("odom", Odometry)
+    tf_publisher = tf.TransformBroadcaster()
+    last_pos = (0,0,0)
+    last_yaw = 0
+    tf_publisher.sendTransform(last_pos,
+                               quaternion_from_euler(0,0,last_yaw),
+                               rospy.Time.now(),
+                               "/odom",
+                               world_frame)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
         
