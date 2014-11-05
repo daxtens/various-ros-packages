d08da8d3f795d7ef699d907e57a473f9cdf0ed6f
Daniel Axtens <daniel@axtens.net>
2014-11-05 14:09:55 +1100

    clean up tree_fix

--- tree_fix/src/tree_fix.py
+++ tree_fix/src/tree_fix.py
@@ -14,7 +14,7 @@ from tf.transformations import quaternion_from_euler
 
 # how far away can two trees be before we decide they're actually
 # two trees and not one tree that's moved. Used in early section only.
-SAME_TREE_THRESH_MATCH = 2
+SAME_TREE_THRESH_MATCH = 3
 
 # how far can a tree be from existing trees after stabilisation
 # before we conclude it's a new tree
@@ -22,7 +22,7 @@ SAME_TREE_THRESH_DETECT = 2
 
 # If tree trunks move by up to this much we update our static points
 # so that the next scan has a lower error.
-SAME_TREE_THRESH_UPDATE=0.3
+SAME_TREE_THRESH_UPDATE=0.0
 
 # the angular window either side of 0 to consider
 SCAN_MATCH_WINDOW = 30 * 3.1415/180
@@ -86,7 +86,6 @@ odom_publisher = None
 static_pts = []
 last_pos = None
 last_yaw = None
-
 world_frame = '/world'
 
 def callback(trees_scan):
@@ -111,7 +110,7 @@ def callback(trees_scan):
                              PointStamped(trees_scan.header,
                                           Point(p[0],p[1],0)))
                          for p in scan_points]
-    
+
 
     # preconvert the points with the last transform
     # it was probably pretty close; we'll only have to find the 
@@ -125,7 +124,7 @@ def callback(trees_scan):
     rotated = R*scan_pts_in_world_xy.T
     translated = rotated + numpy.tile(t, (1, len(scan_pts_in_world)))
     scan_pts_in_world_xy = numpy.array(translated.T)
-                                          
+
     # attempt to match the points to the static scan
     # what we're really trying to do is get a set of shared points
     # between the static scan and the new scan that we can use to
@@ -138,6 +137,7 @@ def callback(trees_scan):
     for p in scan_pts_in_world_xy:
         found = False
         mindist = SAME_TREE_THRESH_MATCH
+        bestptidx = None
         for (i, sp) in enumerate(static_pts):
             #print(p,sp)
             dist = distance(p, sp)
@@ -145,10 +145,13 @@ def callback(trees_scan):
                 print('already used sp', i)
             if dist < mindist and not used[i]:  
                 found = True
+                if bestptidx is not None:
+                    used[bestptidx] = False
                 bestpoint = sp
+                bestptidx = i
                 mindist = dist
                 used[i] = True
-        
+
         if found:
             common_sp += [bestpoint]
             common_np += [p]
@@ -178,8 +181,8 @@ def callback(trees_scan):
         staticp = numpy.matrix(common_sp)
         newp = numpy.matrix(common_np)
         R, t = rototransform2D(newp, staticp)
-        
-                    
+
+
     # now we transform all the world points with R and t
     scan_pts_in_world_xy = numpy.matrix(scan_pts_in_world_xy)
 
@@ -210,36 +213,14 @@ def callback(trees_scan):
         rmse = error(corrected_common_new_pts,
                      numpy.matrix(common_sp))
 
-        rmse_uncorr = error(numpy.matrix(common_np),
-                            numpy.matrix(common_sp))
+        # debug: check if uncorrected has lower error
+        #rmse_uncorr = error(numpy.matrix(common_np),
+        #                    numpy.matrix(common_sp))
         #print(rmse, rmse_uncorr)
-        if rmse > rmse_uncorr:
-            print(rmse,rmse_uncorr)
+        #if rmse > rmse_uncorr:
+        #    print(rmse,rmse_uncorr)
 
-    ## we're occasionally experiencing big jumps
-    ## if we get one, drop this entire match
-    normt = numpy.linalg.norm(t)
     yaw = math.atan2(R[1,0],R[0,0])
-    #print(normt, rmse)
-    #if normt > 10 or rmse > 4 or yaw > 3.4/3:
-    drop=False
-    if normt > 10:
-        print('n')
-        drop = True
-    if rmse > 10:
-        print('e')
-        drop = True
-    if abs(yaw) > 3.14/2:
-        print('y')
-        drop = True
-    if drop:
-        tf_publisher.sendTransform(last_pos,
-                                   quaternion_from_euler(0,0,last_yaw),
-                                   rospy.Time.now(),
-                                   '/odom',
-                                   world_frame)
-        #print('d', rospy.Time.now()-start)
-        return
 
     # detect new/moved trees
     for p in numpy.array(corrected_pts_xy):
@@ -253,7 +234,7 @@ def callback(trees_scan):
             if dist < SAME_TREE_THRESH_DETECT:
                 found = True
                 break
-        
+
         if not found:
             print('new tree at: ', p)
             static_pts += [p]
@@ -293,7 +274,7 @@ def callback(trees_scan):
                            0,    0,    0,     0,     0,     1e-2]
 
     odom_publisher.publish(msg)
-    
+
     # TF hierarchy (per REP 105, iirc) is:
     #     map -> odom -> base_link
     # we want to publish a transform that is map -> odom
@@ -312,12 +293,11 @@ def callback(trees_scan):
     new_yaw = math.atan2(new_R[1,0], new_R[0,0])
     new_pos = (last_pos[0] + t[0,0], last_pos[1] + t[1,0],0)
 
-    last_pos = ((last_pos[0]+new_pos[0])/2,
-                (last_pos[1]+new_pos[1])/2,
-                0)
+    last_pos = numpy.array([(last_pos[0]+new_pos[0])/2,
+                            (last_pos[1]+new_pos[1])/2,
+                            0])
     last_yaw = (new_yaw+last_yaw)/2
-    #last_pos = new_pos
-    #last_yaw = new_yaw
+
     tf_publisher.sendTransform(last_pos,
                                quaternion_from_euler(0,0,last_yaw),
                                rospy.Time.now(),
@@ -336,7 +316,7 @@ def listener():
     tf_listener = tf.TransformListener()
     odom_publisher = rospy.Publisher("odom", Odometry)
     tf_publisher = tf.TransformBroadcaster()
-    last_pos = (0,0,0)
+    last_pos = numpy.array([0,0,0])
     last_yaw = 0
     tf_publisher.sendTransform(last_pos,
                                quaternion_from_euler(0,0,last_yaw),
@@ -345,21 +325,6 @@ def listener():
                                world_frame)
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
-        
+
 if __name__ == '__main__':
     listener()
-
-
-#def talker():p
-#    pub = rospy.Publisher('height', Float32, queue_size=10)
-#    rospy.init_node('scan_to_height', anonymous=True)
-#    while not rospy.is_shutdown():
-#        str = "hello world %s"%rospy.get_time()
-#        rospy.loginfo(str)
-#        pub.publish(str)
-#        r.sleep()
-        
-#if __name__ == '__main__':
-#    try:
-#        talker()
-#    except rospy.ROSInterruptException: pass
